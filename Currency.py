2caca8cd780e373ac141f4a9cb05cf3267c5c18b
Trisha <ptaylor403@gmail.com>
2016-04-04 15:46:36 -0400

    adding currency

--- Currency.py
+++ Currency.py
@@ -2,11 +2,17 @@
 
 class Currency:
     def __init__(self, amount, currency_code):
-        self.code = currency_code
         self.amount = amount
+        self.currency_code = currency_code
 
     def __eq__(self, other):
         if currency_code == other.currency_code and amount == other.amount:
             return True
+        else:
+            return False
+
+    def __add__(self, other):
+        if self.currency_code == other.currency_code:
+            return(self.amount + other.amount)
         else:
             return False
--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,4 +1,12 @@
 # currency converter
+from Currency import *
 
 class CurrencyConverter(Currency):
-    
+    def __init__(self, currency_codes):
+        self.currency_codes = currency_codes
+
+    def convert(self, rate, currency_code):
+        pass
+
+class UnknownCurrencyCodeError(Exception):
+    pass
